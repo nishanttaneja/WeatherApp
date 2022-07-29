6d30a82af1c0b88f467a6994c0c4150e07955744
Nishant Taneja <nishant.taneja2701@gmail.com>
2022-07-29 16:55:39 +0530

    add backgroundview

---
+++ WeatherApp/Screens/Weather Screen/Views/Background View/WeatherBackgroundView.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherBackgroundView.swift
+//  WeatherApp
+//
+//  Created by Nishant Taneja on 29/07/22.
+//
+
+import UIKit
+
+class WeatherBackgroundView: CustomView {
+    override func config() {
+        super.config()
+        backgroundColor = .darkGray
+    }
+}
--- WeatherApp/Screens/Weather Screen/WeatherViewController.swift
+++ WeatherApp/Screens/Weather Screen/WeatherViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 final class WeatherViewController: CustomViewController {
     private let padding = UIEdgeInsets(top: 8, left: 16, bottom: 8, right: 16)
     
+    private let backgroundView = WeatherBackgroundView()
     private let titleView: WeatherTitleView = {
         let titleView = WeatherTitleView()
         titleView.setWeather(temperature: "29°", condition: "Cloudy", forCity: "New Delhi")
@@ -24,9 +25,17 @@ final class WeatherViewController: CustomViewController {
     
     override func config() {
         super.config()
+        backgroundView.translatesAutoresizingMaskIntoConstraints = false
         stackView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(backgroundView)
         view.addSubview(stackView)
         NSLayoutConstraint.activate([
+            // BackgroundView
+            backgroundView.topAnchor.constraint(equalTo: view.topAnchor),
+            backgroundView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            backgroundView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            backgroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            // StackView
             stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: padding.top),
             stackView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: padding.left),
             stackView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -padding.right),
